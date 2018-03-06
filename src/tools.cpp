466c5ce6a1a18c95f904a97a7b92b5a950166b31
Moritz <moritz.untersperger@gmail.com>
2018-03-06 15:54:04 +0100

    updated

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,23 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+
+	VectorXd rmse(4);
+	rmse << 0,0,0,0;
+
+	if(estimations.size() != ground_truth.size()|| estimations.size()==0){
+		cout<< "Invalid";
+		return rmse;
+	}
+
+	for(unsigned int i=0;i<estimations.size();++i){
+		VectorXd residual = estimations[i] -ground_truth[i];
+
+		residual = residual.array()*residual.array();
+		rmse += residual;
+	}
+	rmse = rmse/estimations.size();
+	rmse = rmse.array().sqrt();
+
+	return rmse;
 }
